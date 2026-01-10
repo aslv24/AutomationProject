e02c86359a75cf9a3bf6716bd6738dd5b6c94eae
Mohamed <mohamed@gmail.com>
2026-01-10 11:42:25 +0530

    ReInitialize the WebDriver

--- AutomationProject/src/main/java/com/ap/TestBase/BaseClass.java
+++ AutomationProject/src/main/java/com/ap/TestBase/BaseClass.java
@@ -1,11 +1,12 @@
 package com.ap.TestBase;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseClass {
 	
-	public static WebDriver driver;
-	
+	public static WebDriver driver = new ChromeDriver();
+
 	
 
 }
